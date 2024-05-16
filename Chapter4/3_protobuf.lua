7a5b7820c7b654bbf76bbe3697e641075b78f0d4
Zeyu Liu <274756738@qq.com>
2024-05-16 18:27:38 +0800

    utf-8 encoding

--- Chapter4/3_protobuf.lua
+++ Chapter4/3_protobuf.lua
@@ -2,17 +2,17 @@ local skynet = require "skynet"
 local pb = require "protobuf"
 
 
---protobuf±àÂë½âÂë
+--protobufç¼‚æ «çˆœç‘™ï½‡çˆœ
 function test4()
     pb.register_file("./proto/login.pb")
-    --±àÂë
+    --ç¼‚æ «çˆœ
     local msg = {
         id = 101,
         pw = "123456",
     }
     local buff = pb.encode("login.Login", msg)
     print("len:"..string.len(buff))
-    --½âÂë
+    --ç‘™ï½‡çˆœ
     local umsg = pb.decode("login.Login", buff)
     if umsg then
         print("id:"..umsg.id)
